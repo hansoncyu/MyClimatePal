875dfd2136d8e45bc0582e90bd6960045500087c
Hanson Yu <hansoncyu@gmail.com>
2017-07-10 22:43:16 -0400

    Changed db to mLab

--- Db/server.js
+++ Db/server.js
@@ -5,9 +5,11 @@ var http = require('http');
 var path = require('path');
 
 // start up server, database and models
+
+var mLab = 'mongodb://heroku_mt5smf1m:626dhf48f2731ntrnsd15ncmb8@ds151752.mlab.com:51752/heroku_mt5smf1m'
 var app = express();
 var db = 'mongodb://localhost:27017/app'
-require('./server/models/models.js')(wagner, db);
+require('./server/models/models.js')(wagner, mLab || db);
 
 
 // use api and static files
